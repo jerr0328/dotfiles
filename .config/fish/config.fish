54d209bff9af8fbf2bb54b8f89b7003d2c93080d
Jeremy Mayeres <jeremy@planet.com>
2018-08-06 09:59:18 +0200

    ✨ Add brewupclean alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 rvm default
 alias unset 'set --erase'
+alias brewupclean 'brew update; and brew upgrade; and brew cleanup'
 
 set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/libxml2/bin" $fish_user_paths
