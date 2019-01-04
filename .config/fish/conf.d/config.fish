69048eca7b659c8635693ad90e04769a03f95522
Jeremy Mayeres <mayeres@architrave.de>
2019-01-04 17:34:16 +0100

    ✨ Update latest dotfiles from work, add custom git commands

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -1,4 +1,4 @@
-set -gx PATH $PATH /usr/local/sbin /usr/local/opt/go/libexec/bin
+set fish_user_paths $HOME/bin $fish_user_paths /usr/local/sbin /usr/local/opt/go/libexec/bin
 set -gx LC_ALL en_IE.UTF-8
 set -gx LANG en_IE.UTF-8
 set -gx GOPATH $HOME/work
--- .config/fish/config.fish
+++
@@ -1,3 +0,0 @@
-# rvm default
-set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
