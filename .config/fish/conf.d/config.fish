dff05839593d229d753a7e031994b7adc7fe202a
Jeremy Mayeres <1524722+jerr0328@users.noreply.github.com>
2020-03-22 11:00:02 +0100

    ✨ Rely on pyenv/rbenv plugins, move macos-specific configs

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -1,7 +1,4 @@
-set fish_user_paths $HOME/bin $HOME/.pyenv/bin $HOME/.pyenv/shims $HOME/.rbenv/shims $fish_user_paths /usr/local/sbin /usr/local/opt/go/libexec/bin
+set fish_user_paths $HOME/bin $fish_user_paths /usr/local/sbin
 set -gx LC_ALL en_IE.UTF-8
 set -gx LANG en_IE.UTF-8
 set -gx GOPATH $HOME/work
-set -gx LDFLAGS "-L/usr/local/opt/zlib/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/zlib/include"
-set -gx PKG_CONFIG_PATH "/usr/local/opt/zlib/lib/pkgconfig"
---
+++ .config/fish/conf.d/macos.fish
@@ -0,0 +1,5 @@
+set fish_user_paths $fish_user_paths /usr/local/opt/go/libexec/bin
+set -gx LDFLAGS "-L/usr/local/opt/zlib/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/zlib/include"
+set -gx PKG_CONFIG_PATH "/usr/local/opt/zlib/lib/pkgconfig"
+
