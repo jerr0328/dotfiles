19e5207bf3e8e2b12065a6ab7cf4e782f0003a05
Jeremy Mayeres <jeremy@jerr.dev>
2020-10-21 15:08:45 +0300

    ✨ Add Linuxbrew support

--- .config/fish/conf.d/linux.fish
+++ .config/fish/conf.d/linux.fish
@@ -1,1 +1,7 @@
 set fish_user_paths $HOME/.pyenv/bin $HOME/.rbenv/bin $fish_user_paths
+if test -e ~/.linuxbrew/bin/brew
+    eval (~/.linuxbrew/bin/brew shellenv)
+end
+if test -e /home/linuxbrew/.linuxbrew/bin/brew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
