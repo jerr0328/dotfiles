f09bbd0d1e49f3cae19925d815979a6fc3e48503
Jeremy Mayeres <jeremy@jerr.dev>
2020-12-30 18:25:02 +0100

    Update config from Linux

--- .config/fish/conf.d/linux.fish
+++ .config/fish/conf.d/linux.fish
@@ -1,7 +1,9 @@
-set fish_user_paths $HOME/.pyenv/bin $HOME/.rbenv/bin $fish_user_paths
+set fish_user_paths $HOME/.krew/bin $HOME/.pyenv/bin $HOME/.rbenv/bin $fish_user_paths
 if test -e ~/.linuxbrew/bin/brew
     eval (~/.linuxbrew/bin/brew shellenv)
 end
 if test -e /home/linuxbrew/.linuxbrew/bin/brew
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
+set -x GPG_TTY (tty)
+
